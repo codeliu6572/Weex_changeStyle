eb33934f021b2953ab467e13f5c3e98217c815d4
刘浩浩 <liuhaohao@jianbing.com>
2018-03-12 19:53:46 +0800

    Weex_changeStyle
    weex修改style的几种方法

---
+++ dist/index.js
@@ -0,0 +1,226 @@
+// { "framework": "Vue"} 
+
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 2);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */,
+/* 1 */,
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+var __vue_exports__, __vue_options__
+var __vue_styles__ = []
+
+/* styles */
+__vue_styles__.push(__webpack_require__(3)
+)
+
+/* script */
+__vue_exports__ = __webpack_require__(4)
+
+/* template */
+var __vue_template__ = __webpack_require__(6)
+__vue_options__ = __vue_exports__ = __vue_exports__ || {}
+if (
+  typeof __vue_exports__.default === "object" ||
+  typeof __vue_exports__.default === "function"
+) {
+if (Object.keys(__vue_exports__).some(function (key) { return key !== "default" && key !== "__esModule" })) {console.error("named exports are not supported in *.vue files.")}
+__vue_options__ = __vue_exports__ = __vue_exports__.default
+}
+if (typeof __vue_options__ === "function") {
+  __vue_options__ = __vue_options__.options
+}
+__vue_options__.__file = "/Users/Codeliu/Desktop/weex练习/小功能/changestyle/src/index.vue"
+__vue_options__.render = __vue_template__.render
+__vue_options__.staticRenderFns = __vue_template__.staticRenderFns
+__vue_options__._scopeId = "data-v-5c322ab5"
+__vue_options__.style = __vue_options__.style || {}
+__vue_styles__.forEach(function (module) {
+  for (var name in module) {
+    __vue_options__.style[name] = module[name]
+  }
+})
+if (typeof __register_static_styles__ === "function") {
+  __register_static_styles__(__vue_options__._scopeId, __vue_styles__)
+}
+
+module.exports = __vue_exports__
+module.exports.el = 'true'
+new Vue(module.exports)
+
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports) {
+
+module.exports = {
+  "wrapper": {
+    "width": "200",
+    "height": "200",
+    "backgroundColor": "#FF0000"
+  },
+  "btn": {
+    "justifyContent": "space-between",
+    "alignItems": "center",
+    "flexDirection": "row",
+    "paddingTop": "40",
+    "paddingRight": "40",
+    "paddingBottom": "40",
+    "paddingLeft": "40"
+  }
+}
+
+/***/ }),
+/* 4 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+var animation = weex.requireModule('animation');
+exports.default = {
+  data: function data() {
+    return {};
+  },
+
+  methods: {
+    changeColor: function changeColor() {
+      document.getElementById('haha').style.backgroundColor = 'blue';
+    },
+    reset: function reset() {
+      document.getElementById('haha').style.backgroundColor = 'red';
+      document.getElementById('haha').style = {
+        backgroundColor: 'red',
+        width: '200px,',
+        height: '200px'
+      };
+    },
+    changeWidth: function changeWidth() {
+      animation.transition(this.$refs.haha, {
+        styles: {
+          width: '400px'
+        },
+        duration: 0,
+        timingFunction: 'ease',
+        delay: 0
+      }, function () {});
+    }
+  }
+
+};
+
+/***/ }),
+/* 5 */,
+/* 6 */
+/***/ (function(module, exports) {
+
+module.exports={render:function (){var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;
+  return _c('div', [_c('div', {
+    ref: "haha",
+    staticClass: ["wrapper"],
+    attrs: {
+      "id": "haha"
+    }
+  }), _c('div', {
+    staticClass: ["btn"]
+  }, [_c('text', {
+    on: {
+      "click": _vm.changeColor
+    }
+  }, [_vm._v("改变颜色")]), _c('text', {
+    on: {
+      "click": _vm.reset
+    }
+  }, [_vm._v("重置")]), _c('text', {
+    on: {
+      "click": _vm.changeWidth
+    }
+  }, [_vm._v("改变长度")])])])
+},staticRenderFns: []}
+module.exports.render._withStripped = true
+
+/***/ })
+/******/ ]);
---
+++ src/index.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <div class="wrapper" id="haha" ref="haha">
+
+    </div>
+    <div class="btn">
+      <text @click="changeColor">改变颜色</text>
+      <text @click="reset">重置</text>
+      <text @click="changeWidth">改变长度</text>
+    </div>
+  </div>
+
+
+</template>
+
+<script>
+const animation = weex.requireModule('animation')
+export default {
+  
+  data () {
+    return {
+    }
+  },
+  methods: {
+    changeColor() {
+      document.getElementById('haha').style.backgroundColor = 'blue'
+    },
+    reset() {
+      document.getElementById('haha').style.backgroundColor = 'red'
+      document.getElementById('haha').style = {
+        backgroundColor : 'red',
+        width : '200px,',
+        height : '200px'
+      }
+    },
+    changeWidth() {
+      animation.transition(this.$refs.haha,{
+        styles:{
+          width: '400px'
+        },
+        duration: 0,
+        timingFunction: 'ease',
+        delay: 0
+      }, function(){
+
+      })
+    }
+  
+  }
+
+}
+</script>
+
+<style scoped>
+  .wrapper{
+    width: 200px;
+    height: 200px;
+    background-color: red;
+  }
+  .btn{
+    justify-content: space-between;
+    align-items: center;
+    flex-direction: row;
+    padding: 40px;
+  }
+
+</style>
